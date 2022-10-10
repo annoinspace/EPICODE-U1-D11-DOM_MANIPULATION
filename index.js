ba48ce36294a0d8728852e9a37c9923051af3357
annoinspace <aneesahalmask@gmail.com>
2022-10-10 15:12:02 +0100

    JS 1 & 2
    header and background colour change

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-function changeHeader() {
-  let headerNode = document.querySelector("h1")
-  console.log(headerNode)
+function changeH1Text() {
+  let headerChange = (document.querySelector("h1").innerText =
+    "Your no.1 stop for Smartphones!")
 }
+
+changeH1Text()
+
+function changeBackgroundColour() {
+  let BackgroundColour = (document.querySelector("body").style.backgroundColor =
+    "lightpink")
+}
+
+changeBackgroundColour()
